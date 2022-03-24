7d4ac0a420f8b8565eede2ed2b71c7caf5a3584b
EmoonX <gustavo.chicato@usp.br>
2022-03-24 09:13:13 -0300

    `standardize_size`: ensure equal image size

--- src/img.rs
+++ src/img.rs
@@ -1,4 +1,4 @@
-use image::{io::Reader, DynamicImage, ImageFormat};
+use image::{io::Reader, DynamicImage, ImageFormat, GenericImageView, imageops::FilterType::Triangle};
 
 /// Enum holding image data error type.
 #[derive(Debug)]
@@ -16,4 +16,26 @@ pub fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
     let image = reader.decode().unwrap();
 
     (image, format)
+}
+
+/// Resize biggest image to fit smallest's size.
+pub fn standardize_size(image_1: &DynamicImage, image_2: &DynamicImage) {
+    let smallest = get_smallest_image(image_1, image_2);
+    if smallest == image_1 {
+        image_2.resize_exact(
+            smallest.width(), smallest.height(), Triangle);
+    } else {
+        image_1.resize_exact(
+            smallest.width(), smallest.height(), Triangle);
+    }
+}
+
+/// Compare images by total number of pixels.
+/// Return reference to smallest one.
+fn get_smallest_image<'a>(image_1: &'a DynamicImage, image_2: &'a DynamicImage)
+        -> &'a DynamicImage {
+    let (dim_1, dim_2) = (image_1.dimensions(), image_2.dimensions());
+    let pix_1 = dim_1.0 * dim_1.1;
+    let pix_2 = dim_2.0 * dim_2.1;
+    if pix_1 < pix_2 { image_1 } else { image_2 }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod args;
 mod img;
 
 use args::Args;
-use crate::img::{find_image_from_path, ImageDataErrors};
+use crate::img::{find_image_from_path, ImageDataErrors, standardize_size};
 
 fn main() -> Result<(), ImageDataErrors> {
     // Print command line arguments
@@ -10,14 +10,13 @@ fn main() -> Result<(), ImageDataErrors> {
     println!("{:?}", args);
 
     // Read images from arg paths
-    let (image_1, format_1) =
-        find_image_from_path(args.image_1);
-    let (image_2, format_2) =
-        find_image_from_path(args.image_2);
+    let (image_1, format_1) = find_image_from_path(args.image_1);
+    let (image_2, format_2) = find_image_from_path(args.image_2);
     
     // Assert both images have the same format
     if format_1 != format_2 {
         return Err(ImageDataErrors::DifferentImageFormats);
     }
+    standardize_size(&image_1, &image_2);
     Ok(())
 }
